a1ff2aaf3699e61a9065878ae12ecff32fcc69bd
Bogban893 <bogbanrvd@gmail.com>
2026-09-16 15:04:40 +0300

    feat: add get_graph_info

--- project/graph.py
+++ project/graph.py
@@ -1,4 +1,5 @@
 from typing import NamedTuple, Any
+import cfpq_data
 
 
 
@@ -6,6 +7,17 @@ class GraphInfo(NamedTuple):
     number_of_nodes: int
     number_of_edges: int
     labels: set[str]
+
+
+def get_graph_info(graph_name: str) -> GraphInfo:
+    graph_path = cfpq_data.download(graph_name)
+    graph = cfpq_data.graph_from_csv(graph_path)
+
+    nodes_cnt = graph.number_of_nodes()
+    edges_cnt = graph.number_of_edges()
+    labels = set(cfpq_data.get_sorted_labels(graph))
+
+    return GraphInfo(nodes_cnt, edges_cnt, labels)
 
 
 
